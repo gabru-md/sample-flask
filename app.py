68a0a329bf73d22848c1bc87aab69f516ca4db82
Manish Devgan <manish.nsit8@gmail.com>
2024-03-26 23:22:41 +0100

    Add Mongo db support for counter

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import Flask, url_for
 from flask import render_template
 from werkzeug.utils import redirect
@@ -5,6 +7,47 @@ from werkzeug.utils import redirect
 app = Flask(__name__)
 
 counter = {}
+
+from pymongo.mongo_client import MongoClient
+from urllib.parse import quote_plus
+import os
+
+username = quote_plus(os.getenv('mongodb_username'))
+password = quote_plus(os.getenv('mongodb_password'))
+cluster = os.getenv('mongodb_cluster')
+
+uri = 'mongodb+srv://' + username + ':' + password + '@' + cluster + '/?retryWrites=true&w=majority&appName=Auto-Skribbl-Cluster'
+
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    db = client.get_database('counter')
+    counter_collection = db.get_collection('counter')
+    stats_collection = db.get_collection('stats')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+
+def get_counter_value(keyword):
+    counter = counter_collection.find_one({"keyword": keyword})
+    if counter:
+        return counter["count"]
+    else:
+        counter_collection.insert_one({"keyword": keyword, "count": 0})
+        return 0
+
+
+def increment_counter(keyword):
+    old_value = get_counter_value(keyword)
+    counter_collection.update_one({"keyword": keyword}, {"$set": {"count": old_value + 1}})
+    return old_value + 1
+
+
+def add_stat_for_keyword(keyword):
+    stats_collection.insert_one({"keyword": keyword, "date": datetime.now()})
 
 
 @app.route("/")
@@ -14,18 +57,21 @@ def hello_world():
 
 @app.route("/count/<keyword>", methods=["GET"])
 def count(keyword):
-    return render_template("count.html", keyword=keyword, count=counter.get(keyword, 0))
+    counter_value = get_counter_value(keyword)
+    return render_template("count.html", keyword=keyword, count=counter_value)
 
 
 @app.route("/count/<keyword>", methods=["POST"])
 def count_increment(keyword):
-    counter[keyword] = counter.get(keyword, 0) + 1
+    increment_counter(keyword)
+    add_stat_for_keyword(keyword)
     return redirect(url_for("count", keyword=keyword), code=302)
 
 
 @app.route("/count", methods=["GET"])
 def all_counters():
-    return render_template("all_counters.html", counters=counter)
+    leaderboard = counter_collection.find().sort([('count', -1)]).limit(20)
+    return render_template("all_counters.html", counters=leaderboard)
 
 
 if __name__ == '__main__':
